6b04b4ac30f5962ab93ea3b51a26f949f2a0253e
Grant Slape <me@grantslape.com>
2018-09-08 17:58:13 -0500

    Create tables with silly formatting in column names

--- mysql/hw1-create.sql
+++ mysql/hw1-create.sql
@@ -1,29 +1,36 @@
 CREATE DATABASE IF NOT EXISTS sales;
 
-CREATE TABLE IF NOT EXISTS retail_order (
-  order_number int not null,
-  store_number int not null,
-  store_zip char(9) not null,
-  order_month char(12) not null,
-  order_year int not null,
-  order_total decimal(15,2) not null,
-  PRIMARY KEY (order_number)
+USE sales;
+
+CREATE TABLE IF NOT EXISTS RETAIL_ORDER (
+  OrderNumber int not null,
+  StoreNumber int not null,
+  StoreZIP char(9) not null,
+  OrderMonth char(12) not null,
+  OrderYear int not null,
+  OrderTotal decimal(15,2) not null,
+  PRIMARY KEY (OrderNumber)
 ) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS sku_data (
-  sku int not null,
-  sku_description char(35) not null,
-  department char(30) not null,
-  buyer char(30),
-  PRIMARY KEY (sku)
+  SKU int not null,
+  SKU_Description char(35) not null,
+  Department char(30) not null,
+  Buyer char(30),
+  PRIMARY KEY (SKU)
 ) ENGINE = INNODB;
 
-  CREATE TABLE IF NOT EXISTS order_item (
-    order_number int not null,
-    sku int not null,
-    quantity int not null,
-    price decimal(15,2) not null,
-    extended_price decimal(15,2) not null,
-    CONSTRAINT order_itemfk_1 FOREIGN KEY (order_number) REFERENCES retail_order(order_number) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT order_itemfk_2 FOREIGN KEY (sku) REFERENCES  sku_data(sku) ON DELETE CASCADE ON UPDATE CASCADE
-  ) ENGINE = INNODB;
+CREATE TABLE IF NOT EXISTS order_item (
+  OrderNumber int not null,
+  SKU int not null,
+  Quantity int not null,
+  Price decimal(15,2) not null,
+  ExtendedPrice decimal(15,2) not null,
+  CONSTRAINT order_itemfk_1 FOREIGN KEY (OrderNumber) REFERENCES retail_order(OrderNumber) 
+  ON DELETE CASCADE 
+  ON UPDATE CASCADE,
+  CONSTRAINT order_itemfk_2 FOREIGN KEY (SKU) REFERENCES  sku_data(SKU) 
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+) ENGINE = INNODB;
+
