742b1ecf1c9a9e03291ba03fd74f21b08c372da9
Grant Slape <me@grantslape.com>
2018-09-24 20:39:25 -0500

    create file for #4

---
+++ mysql/HW4/hw4-create.sql
@@ -0,0 +1,40 @@
+DROP SCHEMA IF EXISTS morgan_importing;
+
+CREATE SCHEMA morgan_importing;
+
+use morgan_importing;
+
+CREATE TABLE item (
+  ItemID int not null AUTO_INCREMENT,
+  Description varchar(255) not null,
+  PurchaseDate date not null,
+  Store char(50) not null,
+  City char(35) not null,
+  Quantity int not null,
+  LocalCurrencyAmount decimal(18,2) not null,
+  ExchangeRate decimal(12,6) not null
+  PRIMARY KEY (ItemID)
+)ENGINE = INNODB;
+
+CREATE TABLE shipment (
+  ShipmentID int not null AUTO_INCREMENT,
+  ShipperName char(35) not null,
+  ShipperInvoiceNumber int not null,
+  DepartureDate date,
+  ArrivalDate date,
+  InsuredValue decimal(12,2),
+  PRIMARY KEY (ShipmentID)
+)ENGINE = INNODB;
+
+CREATE TABLE shipment_item (
+  ShipmentID int not null,
+  ShipmentItemID int not null,
+  ItemID into not null,
+  Values decimal(12,2) not null,
+  PRIMARY KEY (ShipmentID, ShipmentItemID),
+  CONSTRAINT shipment_itemfk_1 FOREIGN KEY (ShipmentID) REFERENCES shipment(ShipmentID)
+  ON UPDATE CASCADE,
+  CONSTRAINT shipment_itemfk_2 FOREIGN KEY (ItemID) REFERENCES item(ItemID)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+)ENGINE = INNODB;
